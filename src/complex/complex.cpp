c953e0bac40710d1bc258625780e8da6e0ab0762
Temosho Mathabatha <mathabathatemosho@gmail.com>
2024-08-05 19:37:08 +0200

    Exercise 3.2

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -23,7 +23,7 @@ int main()
 
 	// Exercise 3.2
 
-	// EXERCISE 3.3, I AM USING COMPLE(FLOAT)
+	// EXERCISE 3.2, I AM USING COMPLE(FLOAT)
 	// Thius line is important that it can .......
 	return 0;
 }
